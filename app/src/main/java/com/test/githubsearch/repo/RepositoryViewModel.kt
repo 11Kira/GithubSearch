0475fdb08d838cf63c724b8e3453f3f343d224f7
JULIUS <kiravillagracia@yahoo.com>
2021-07-07 03:27:14 +0800

    #viewmodel-module: created ui event for repository viewmodel

--- app/src/main/java/com/test/githubsearch/repo/RepositoryViewModel.kt
+++ app/src/main/java/com/test/githubsearch/repo/RepositoryViewModel.kt
@@ -1,3 +1,16 @@
 package com.test.githubsearch.repo
 
-class RepositoryViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.test.githubsearch.core.BaseViewModel
+import com.test.githubsearch.core.UIEvent
+import org.koin.core.component.inject
+
+class RepositoryViewModel : BaseViewModel() {
+
+    private val repositoryResultsRepo: RepositoryResultsRepo by inject()
+
+    private val _events = MutableLiveData<UIEvent>()
+    val events: LiveData<UIEvent> = _events
+
+}
