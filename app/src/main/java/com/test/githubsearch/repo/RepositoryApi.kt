63313e7b34e1ababeb152835bdeffaa1d8ae9fdf
JULIUS <kiravillagracia@yahoo.com>
2021-07-07 02:45:57 +0800

    #api-models: added interface for repositoryApi

---
+++ app/src/main/java/com/test/githubsearch/repo/RepositoryApi.kt
@@ -0,0 +1,14 @@
+package com.test.githubsearch.repo
+
+import com.test.githubsearch.response.ApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RepositoryApi {
+    @GET("search")
+    suspend fun searchRepositories(
+        @Query("q") searchQuery: String?,
+        @Query("type") type: String?,
+    ): Response<ApiResponse<List<Repository>>>
+}
