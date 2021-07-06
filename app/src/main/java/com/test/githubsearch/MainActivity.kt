2a90027a403a3d65a8073538b866ef71073504c6
JULIUS <kiravillagracia@yahoo.com>
2021-07-07 00:39:44 +0800

    #activities: initial functions for main activity (recyclerview initialization, databinding)

--- app/src/main/java/com/test/githubsearch/MainActivity.kt
+++ app/src/main/java/com/test/githubsearch/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.test.githubsearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.test.githubsearch.databinding.ActivityMainBinding
 
+/**
+ * Activity class for the repository list
+ * @author Julius Villagracia
+ */
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
+    /**
+     * Initializes the recyclerview and contents
+     */
+    private fun initRecyclerView() {
+        binding.repoList.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            //adapter = beerListAdapter
+        }
+    }
+
 }
