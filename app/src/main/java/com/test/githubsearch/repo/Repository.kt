a7c84a8657b8a342715f1745cf1c8e85e7bc560c
JULIUS <kiravillagracia@yahoo.com>
2021-07-07 02:30:18 +0800

    #api-models: added data classes for repo and owner

---
+++ app/src/main/java/com/test/githubsearch/repo/Repository.kt
@@ -0,0 +1,26 @@
+package com.test.githubsearch.repo
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Repository(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("node_id")
+    val nodeId: Long,
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("full_name")
+    val fullName: String? = "",
+    @SerializedName("private")
+    val private: String? = "",
+    @SerializedName("owner")
+    val owner: Owner? = null,
+    @SerializedName("html_url")
+    val htmlUrl: String? = "",
+    @SerializedName("description")
+    val description: String? = "",
+    @SerializedName("stargazers_count")
+    val stargazersCount: Long
+
+): Serializable
