6ede50a695752bbd76e0b156abf45abf66e23246
JULIUS <kiravillagracia@yahoo.com>
2021-07-07 03:38:25 +0800

    #viewmodel-module: added viewModel function for searching the repositories

--- app/src/main/java/com/test/githubsearch/repo/RepoEvent.kt
+++ app/src/main/java/com/test/githubsearch/repo/RepoEvent.kt
@@ -5,6 +5,6 @@ import com.test.githubsearch.core.UIEvent
 sealed class RepoEvent : UIEvent {
     data class OnStartLoading(val success: Boolean) : RepoEvent()
     data class OnFailedFetching(val error: String) : RepoEvent()
-    data class OnFinishedLoading(val beers: List<Repository>) : RepoEvent()
+    data class OnFinishedLoading(val repositories: List<Repository>) : RepoEvent()
     object OnNoAvailable: RepoEvent()
 }
--- app/src/main/java/com/test/githubsearch/repo/RepositoryViewModel.kt
+++ app/src/main/java/com/test/githubsearch/repo/RepositoryViewModel.kt
@@ -2,8 +2,12 @@ package com.test.githubsearch.repo
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.test.githubsearch.core.BaseViewModel
 import com.test.githubsearch.core.UIEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.core.component.inject
 
 class RepositoryViewModel : BaseViewModel() {
@@ -13,4 +17,23 @@ class RepositoryViewModel : BaseViewModel() {
     private val _events = MutableLiveData<UIEvent>()
     val events: LiveData<UIEvent> = _events
 
+    fun searchRepositories(query: String, type: String) {
+        _events.value = RepoEvent.OnStartLoading(true)
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = kotlin.runCatching { repositoryResultsRepo.searchRepositories(query, type) }
+            withContext(Dispatchers.Main) {
+                result.onSuccess { response ->
+                    response?.items?.let { repoList ->
+                        if (repoList.isEmpty()) {
+                            _events.value = RepoEvent.OnNoAvailable
+                        } else {
+                            _events.value = RepoEvent.OnFinishedLoading(repoList)
+                        }
+                    }
+                }.onFailure { error ->
+                    _events.value = RepoEvent.OnFailedFetching(error.localizedMessage)
+                }
+            }
+        }
+    }
 }
